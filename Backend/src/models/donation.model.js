9ea1ed6af3524ac220aa1e6cae4c64b1b7f07007
The-Wee-Lad <113277834+The-Wee-Lad@users.noreply.github.com>
2025-03-12 01:10:34 +0530

    add donation model

--- Backend/src/models/donation.model.js
+++ Backend/src/models/donation.model.js
@@ -1,7 +1,28 @@
 import mongoose,{ Schema,model } from "mongoose";
 
+
+
 const donationSchema = new Schema({
-    
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:"users"
+    },
+    ngo:{
+        type:mongoose.Types.ObjectId,
+        ref:"ngos"
+    },
+    donationType:{
+        type:String,
+        enum:["monetary","items"]
+    },
+    amount:{
+        type:mongoose.Decimal128,
+        required:[function(){return this.donationType == "monetary"},"Money Is not item type"]
+    },
+    items:{
+        type:[new Schema({itemName:String, quantity:Number, tagId:String})],
+        required:[function(){return this.donationType == "items"},"Item Is not monetary type"]
+    }
 },{timestamps:true});
 
 export const donation =  model("donations",donationSchema);
